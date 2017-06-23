0dae7178c8787ba13fd1db9df7ab805514057ee7
Brian Lai <brian@rideco.com>
2017-06-23 19:36:10 -0400

    what on earth am i doing

---
+++ src/js/index.ts
@@ -0,0 +1,19 @@
+import _ from 'lodash';
+
+(function () {
+    "use strict";
+
+    alert('ayy lmao');
+
+    function component() {
+        var element = document.createElement('div');
+
+        // Lodash, currently included via a script, is required for this line to work
+        element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+        return element;
+    }
+
+    document.body.appendChild(component());
+
+}());
