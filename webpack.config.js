833beaa4acaf1e2f4cb1ef5f726d65536e7d28fb
Brian Lai <brian@rideco.com>
2017-06-23 18:03:58 -0400

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+/*global module,require*/
+
+var path = require('path');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'www')
+    }
+};
