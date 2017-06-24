72e1d641231865d127529542aeba92101c0c72ed
Brian Lai <brian@rideco.com>
2017-06-24 17:59:14 -0400

    lolololol

--- src/js/Content.tsx
+++ src/js/Content.tsx
@@ -3,18 +3,28 @@ import RedditService from './RedditService';
 
 class Content extends Component {
     private props: any;
+    public state: any;
+    public setState: any;
 
     async componentDidMount() {
         let res = await RedditService.get('pics');
-        const posts = res.data.data.children.map(obj => obj.data);
+        const posts = res.data.children.map(obj => obj.data);
         console.log(posts);
         this.setState({posts});
     }
 
     render() {
-        return (
-            <h1>Hello world!</h1>
-        );
+        const self = this;
+        if (this.state && this.state.posts) {
+            return (
+                <div>{
+                    self.state.posts.map(function (post, i) {
+                        return <div>{self.state.posts[i].title}</div>;
+                    })
+                }</div>
+            );
+        }
+        return <div></div>;
     }
 }
 
