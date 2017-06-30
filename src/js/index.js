cebd8ae673151ab7f419e919dabc255b547632e8
Brian Lai <brian@rideco.com>
2017-06-30 14:55:39 -0400

    shit sucks, starting over

--- src/js/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-require("../css/index.styl");
-ReactDOM.render(React.createElement(App, null), document.getElementById('app'));
-//# sourceMappingURL=index.js.map
